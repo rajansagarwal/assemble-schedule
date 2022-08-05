717081895c4300cb4c57f019bbc01bfb52eecf60
Rajan Agarwal <rajan.ag005@gmail.com>
2022-08-05 00:40:10 -0700

    fixed date fetching

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,41 @@
-import Head from 'next/head'
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { scheduleAirtable } from '../lib/airtable'
 
 export default function Home() {
-	// const [events, setEvents] = useState([])
-	const array = []
+	const [events, setEvents] = useState([])
 
 	;(async () => {
 		try {
-			const readEvents = await scheduleAirtable.read({})
+			const array = []
+			const readEvents = await scheduleAirtable.read({
+				view: 'Grid view',
+			})
 			for (let i = 0; i < readEvents.length; i++) {
-				//setEvents((events) => [...events, readEvents[i]])
 				array[i] = readEvents[i].fields
 			}
-			console.log(array)
+			setEvents(array)
 		} catch (e) {
 			console.error(e)
 		}
 	})()
 
 	return (
-		<ol>
-			{array.map((item, index) => {
-				return <li key={index}>{item?.Name}</li>
+		<ul>
+			{events.map((item, index) => {
+				let start = new Date(item['Start Time'])
+				let end = new Date(item['End Time'])
+				return (
+					<li key={index}>
+						<b>
+							{start.getDate() === '5' ? 'Friday' : 'Saturday'} August{' '}
+							{start.getDate()}, {start.getUTCHours()}
+							{start.getUTCMinutes() > 0 ? ':30' : null} - {end.getDate()}
+							{end.getUTCMinutes() > 0 ? ':30' : null}
+						</b>{' '}
+						{item.Name} ({item.Description})
+					</li>
+				)
 			})}
-		</ol>
+		</ul>
 	)
 }
