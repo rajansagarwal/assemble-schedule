3dc0cac85ee0359c88456de965e8618efaa6f0f8
Rajan Agarwal <rajan.ag005@gmail.com>
2022-08-04 21:38:04 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,29 @@
 import Head from 'next/head'
+// import { useState } from 'react'
+import { scheduleAirtable } from '../lib/airtable'
 
 export default function Home() {
+	// const [events, setEvents] = useState([])
+	const array = []
+
+	;(async () => {
+		try {
+			const readEvents = await scheduleAirtable.read({})
+			for (let i = 0; i < readEvents.length; i++) {
+				//setEvents((events) => [...events, readEvents[i]])
+				array[i] = readEvents[i].fields
+			}
+			console.log(array)
+		} catch (e) {
+			console.error(e)
+		}
+	})()
+
 	return (
-		<div>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			Hi there!
-		</div>
+		<ol>
+			{array.map((item, index) => {
+				return <li key={index}>{item?.Name}</li>
+			})}
+		</ol>
 	)
 }
